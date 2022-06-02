c202b32413ad86465bef61bd531efa34c9ab8847
omarrashed1 <omrrashd.25@gmail.com>
2022-06-02 08:13:32 +0300

    Update workout.java

--- app/src/main/java/com/example/gym_trainer/workout.java
+++ app/src/main/java/com/example/gym_trainer/workout.java
@@ -10,5 +10,7 @@ public class workout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        
     }
 }
