3bff371f0d0751fd997fe0af37515791717e9b90
saleeem123 <98378349+saleeem123@users.noreply.github.com>
2022-06-13 08:20:55 +0300

    add
    add

--- app/src/main/java/com/example/gym_trainer/MainActivity.java
+++ app/src/main/java/com/example/gym_trainer/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.gym_trainer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ImageView imagebutton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        getSupportActionBar().hide();
 
+        imagebutton2 = (ImageView) findViewById(R.id.imagebutton2);
+        imagebutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this,workout.class);
+                startActivity(i);
+            }
+        });
 
 
 
