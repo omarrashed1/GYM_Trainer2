05468916ee169e517408b61f9d7e40931475ccfb
saleeem123 <98378349+saleeem123@users.noreply.github.com>
2022-06-16 11:45:06 +0300

    add
    add

--- app/src/main/java/com/example/gym_trainer/MainActivity.java
+++ app/src/main/java/com/example/gym_trainer/MainActivity.java
@@ -10,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imagebutton2;
+    ImageView imagebutton4;
+    ImageView imagebutton5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,30 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportActionBar().hide();
 
         imagebutton2 = (ImageView) findViewById(R.id.imagebutton2);
+        imagebutton4 = (ImageView) findViewById(R.id.imagebutton4);
+        imagebutton5 = (ImageView) findViewById(R.id.imagebutton5);
+
         imagebutton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent i = new Intent(MainActivity.this,workout.class);
+                startActivity(i);
+            }
+        });
+        imagebutton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this,backlunges.class);
+                startActivity(i);
+            }
+        });
+        imagebutton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this,frog.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/gym_trainer/home_page.java
+++ app/src/main/java/com/example/gym_trainer/home_page.java
@@ -12,17 +12,28 @@ import android.widget.ImageView;
 
 public class home_page extends AppCompatActivity {
     ImageView imagebutton2;
+    ImageView imagebutton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         imagebutton2=(ImageView) findViewById(R.id.imagebutton2);
+        imagebutton4=(ImageView) findViewById(R.id.imagebutton4);
+
         imagebutton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent i = new Intent(home_page.this, workout.class);
+                startActivity(i);
+            }
+        });
+        imagebutton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(home_page.this, backlunges.class);
                 startActivity(i);
             }
         });
